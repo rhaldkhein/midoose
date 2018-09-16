15c130b13e512ccd5b1643818e293e1eeb95c53f
rhaldkhein <undyingmind@gmail.com>
2018-09-17 00:13:34 +0400

    ...

--- test/index.js
+++ test/index.js
@@ -6,7 +6,6 @@ const chaiAsPromised = require('chai-as-promised')
 
 require('./models')
 
-// chai.should()
 chai.use(chaiAsPromised)
 chai.use(sinonChai)
 
@@ -20,21 +19,22 @@ global.mockRes = json => {
   }
 }
 
+// Sample data
 const users = [
-  { _id: '123', name: 'FooUser', age: 1, active: true },
-  { _id: '124', name: 'BarUser', age: 2, active: false },
-  { _id: '125', name: 'BazUser', age: 3, active: true },
-  { _id: '126', name: 'ZapUser', age: 4, active: false }
+  { _id: '101', name: 'FooUser', age: 1, active: true },
+  { _id: '102', name: 'BarUser', age: 2, active: false },
+  { _id: '103', name: 'BazUser', age: 2, active: true },
+  { _id: '104', name: 'ZapUser', age: 3, active: false }
 ]
 const posts = [
-  { user: users[0]._id, title: 'FooPost' },
-  { user: users[0]._id, title: 'BarPost' },
-  { user: users[1]._id, title: 'BazPost' },
-  { user: users[2]._id, title: 'ZapPost' },
-  { user: users[2]._id, title: 'JazPost' },
-  { user: users[2]._id, title: 'GalPost' },
-  { user: users[3]._id, title: 'FaaPost' },
-  { user: users[3]._id, title: 'YooPost' }
+  { _id: '201', user: users[0]._id, title: 'FooPost', published: true },
+  { _id: '202', user: users[0]._id, title: 'BarPost', published: false },
+  { _id: '203', user: users[1]._id, title: 'BazPost', published: true },
+  { _id: '204', user: users[2]._id, title: 'ZapPost', published: false },
+  { _id: '205', user: users[2]._id, title: 'JazPost', published: true },
+  { _id: '206', user: users[2]._id, title: 'GalPost', published: true },
+  { _id: '207', user: users[3]._id, title: 'FaaPost', published: false },
+  { _id: '208', user: users[3]._id, title: 'YooPost', published: true }
 ]
 global.samples = { users, posts }
 
--- test/suites/findAll.js
+++ test/suites/findAll.js
@@ -4,42 +4,69 @@ const findAll = require('../../src/middlewares/findAll')
 
 describe('findAll', () => {
 
-  let stub
+  let stubUserFind
+  let stubPostFind
 
   const req = {
     body: {
-      active: true
+      active: true,
+      published: false
     },
     query: {
-      active: false
+      active: false,
+      published: true
     }
   }
 
   before(() => {
-    stub = sinon.stub(Model.User, 'find')
-    stub.withArgs({ active: true }).returns(Promise.resolve(
+
+    /**
+     * User stubs
+     */
+    stubUserFind = sinon.stub(Model.User, 'find')
+    stubUserFind.withArgs({ active: true }).returns(Promise.resolve(
       _filter(samples.users, { active: true })
     ))
-    // stub.withArgs('456').returns(Promise.resolve([
-    //   { _id: '123' }
-    // ]))
+    stubUserFind.withArgs({ active: false }).returns(Promise.resolve(
+      _filter(samples.users, { active: false })
+    ))
+    stubUserFind.withArgs({ active: true, age: 2 }).returns(Promise.resolve(
+      _filter(samples.users, { active: true, age: 2 })
+    ))
     // stub.withArgs('error').returns(
     //   Promise.reject(new Error('sample error'))
     // )
-    stub.returns(Promise.resolve(samples.users))
+    stubUserFind.returns(Promise.resolve(samples.users))
+
+    /**
+     * Post stubs
+     */
+    stubPostFind = sinon.stub(Model.Post, 'find')
+    stubPostFind.withArgs({ published: true }).returns(Promise.resolve(
+      _filter(samples.posts, { published: true })
+    ))
+    stubPostFind.withArgs({ published: false }).returns(Promise.resolve(
+      _filter(samples.posts, { published: false })
+    ))
+    stubPostFind.returns(Promise.resolve(samples.posts))
+
+
   })
 
   after(() => {
-    stub.restore()
+    stubUserFind.restore()
+    stubPostFind.restore()
   })
 
-  it('should find many (default)', done => {
+  it('should find all (default)', done => {
 
     const res = mockRes(payload => {
       try {
-        console.log(payload)
-        // expect(payload).to.be.property('_id')
-        // expect(payload._id).to.be.equal('123')
+        expect(payload).to.have.lengthOf(4)
+        expect(payload[0]._id).to.be.equal('101')
+        expect(payload[1]._id).to.be.equal('102')
+        expect(payload[2]._id).to.be.equal('103')
+        expect(payload[3]._id).to.be.equal('104')
         done()
       } catch (error) {
         done(error)
@@ -50,6 +77,92 @@ describe('findAll', () => {
       Model.User
     )(req, res)
 
+  })
+
+  it('should find all with condition', done => {
+
+    const res = mockRes(payload => {
+      try {
+        expect(payload).to.have.lengthOf(3)
+        expect(payload[0]._id).to.be.equal('202')
+        expect(payload[1]._id).to.be.equal('204')
+        expect(payload[2]._id).to.be.equal('207')
+        done()
+      } catch (error) {
+        done(error)
+      }
+    })
+
+    findAll(
+      Model.Post,
+      { published: false }
+    )(req, res)
+
+  })
+
+  it('should find all with condition from body', done => {
+
+    const res = mockRes(payload => {
+      try {
+        expect(payload).to.have.lengthOf(2)
+        expect(payload[0].name).to.be.equal('FooUser')
+        expect(payload[1].name).to.be.equal('BazUser')
+        done()
+      } catch (error) {
+        done(error)
+      }
+    })
+
+    findAll(
+      Model.User,
+      { active: 'body.active' }
+    )(req, res)
+
+  })
+
+  it('should find all with more condition from body and locals', done => {
+
+    const res = mockRes(payload => {
+      try {
+        expect(payload).to.have.lengthOf(1)
+        expect(payload[0]._id).to.be.equal('103')
+        expect(payload[0].name).to.be.equal('BazUser')
+        done()
+      } catch (error) {
+        done(error)
+      }
+    })
+
+    res.locals.age = 2
+
+    findAll(
+      Model.User,
+      {
+        active: 'body.active',
+        age: 'locals.age'
+      }
+    )(req, res)
+
+  })
+
+  it('should find all with function condition', done => {
+
+    const res = mockRes(payload => {
+      try {
+        expect(payload).to.have.lengthOf(5)
+        expect(payload[4].title).to.be.equal('YooPost')
+        done()
+      } catch (error) {
+        done(error)
+      }
+    })
+
+    findAll(
+      Model.Post,
+      (req) => {
+        return { published: req.query.published }
+      }
+    )(req, res)
 
   })
 
